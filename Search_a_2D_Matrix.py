3213011fdae8875b5819edd535b1a6504fb7c5a7
Shrouk Hegazi <67985725+shroukhegazi@users.noreply.github.com>
2022-07-07 03:59:33 +0200

    Create Search_a_2D_Matrix.py

---
+++ Search_a_2D_Matrix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n = len(matrix)
+        m = len(matrix[0])
+
+        for i in range (n):
+            for j in range (m):
+                if(matrix[i][j]==target):
+                    return True
+                    break
+        return False            
+        
